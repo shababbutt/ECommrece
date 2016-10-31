d6ec95265829fc3a15e618117a40d52c613b88c1
Great Butt <Great Butt@DESKTOP-V0QQHD1>
2016-10-31 13:57:24 +0500

    Add project files.

---
+++ E-Commrece/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="E_Commrece.MvcApplication" Language="C#" %>
---
+++ E-Commrece/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_Commrece.Startup))]
+namespace E_Commrece
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
